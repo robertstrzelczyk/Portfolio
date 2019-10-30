bda1c7a61b3724cc7ad1e0d3341251f2f583d87c
r.strzelczyk <robert.strzelczyk35a@gmail.com>
2019-10-30 17:51:28 +0100

    add a skills section

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+        $('.chart').easyPieChart({
+          size: 140,
+            barColor: '#1094a2',
+            scaleColor: false,
+            lineWidth: 7,
+            trackColor: '#cccccc',
+            lineCap: 'circle',
+            animate: 1500
+        });
+    });
